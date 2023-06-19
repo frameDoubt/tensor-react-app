dfc2828d86cd750964483f81e74a200841792db2
Brandon Wright <brandon.w.wright4@gmail.com>
2023-06-18 19:32:38 -0700

    add fix to feature' return text

--- src/components/CameraDisplay.tsx
+++ src/components/CameraDisplay.tsx
@@ -1,21 +1,23 @@
 import React, { useEffect, useState } from "react";
 
 function CameraDisplay() {
+
+    const strClnr = (str: string): string => { return str.split(' ').slice(0, -1).join(" ") };
     
     let [connectedUserMedia, setConnectedUserMedia] = useState("");
     
     useEffect(() => {
         const getConnectedUserMedia = navigator.mediaDevices.enumerateDevices();
         getConnectedUserMedia.then((response) => { 
-            console.log(response["1"]["label"]);
-            setConnectedUserMedia(currentUM => currentUM = response["1"]["label"]);
+            let retStr = response["1"]["label"]; 
+            setConnectedUserMedia(currentUM => currentUM = strClnr(retStr));
         });
     }
         ,[]);
     return (
         <div>
             <p>This is where I'm going to return my MediaDevices list:</p>
-            {`<p>${connectedUserMedia}<p/>`}
+            <p>{connectedUserMedia}</p>
         </div>
     );
 }
